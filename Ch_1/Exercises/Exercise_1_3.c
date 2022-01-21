df501ff35f9511985532d2a7b0e9db50d275fb82
greenleafvolatile <daanpol@tutamail.com>
2022-01-21 15:38:16 +0100

    Added exercise 1-3.

---
+++ Ch_1/Exercises/Exercise_1_3.c
@@ -0,0 +1,32 @@
+/******************************************************************
+ * Exercise 1-3 Modify the temperature conversion program to print*
+ * a heading above the table.                                     *
+ * @author Daan Pol                                               *
+ * @version 1.0                                                   *
+ * @since 21-01-2022                                              *
+ ******************************************************************/
+#include <stdio.h>
+
+int
+main(void) {
+
+    float fahr, celsius;
+    int  lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+    fahr = lower;
+
+    printf("%-10s %s\n", "Fahrenheit", "Celsius");
+
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32);
+        printf("%-10.0f %.2f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}
+
+	
